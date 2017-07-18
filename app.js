c6099412b41579e16cf1e6c028a3cf65d07623d8
sergioeabarcaf <sergioaf1991@gmail.com>
2017-07-18 11:15:04 -0400

    dev_sergioeabarcaf INSTALL socket.io: Se instala socket.io y se agrega que cuando haya conexion pueda enviar mensaje

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
+var server = require("http").createServer(app);
+var io = require("socket.io").listen(server);
+
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "jade");
 
+io.sockets.on("connection", function(socket){
+  socket.on("sendMessage", function(data){
+    io.socket.emit("newMessage", {msg: data});
+  });
+});
+
 app.get("/",function(req,res){
   res.render("index");
+});
+
+app.get("/chat",function(req,res){
+  res.render("chat");
+});
+
+app.get("/dashboard",function(req,res){
+  res.render("dashboard");
 });
 
 app.listen(8080);

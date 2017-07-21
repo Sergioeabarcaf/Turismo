0e4fd05ec4290f781c16472ea10efc42d56960f5
sergioeabarcaf <sergioaf1991@gmail.com>
2017-07-21 16:34:07 -0400

    dev_sergioeabarcaf UPDATE Mongoose: Se crea un Schema de los datos y se acopla al controlador, se solucionan los errores pero que pendiente la busqueda de los datos guardados en la db

--- app.js
+++ app.js
@@ -1,82 +1,93 @@
 var express = require('express'),
-    app = express(),
-    server = require('http').createServer(app),
-    io = require("socket.io").listen(server),
-    nicknames = {},
-    mqtt = require('mqtt'),
-    client = mqtt.connect('mqtt://localhost:1883')
-    mongoose = require("mongoose")
-    mongoose.connect('mongodb://localhost/estacion');
+	app = express(),
+	server = require('http').createServer(app),
+	io = require("socket.io").listen(server),
+	nicknames = {},
+	mqtt = require('mqtt'),
+	client = mqtt.connect('mqtt://localhost:1883'),
+	Sensor = require("./models/sensor").Sensor;
 
+//datos de prueba de sensores
 var temp = 65.5;
 var id = "sergio ";
 var messageTemp = id + String(temp);
-var idmess = " ";
-var x = " ";
+var splitMessage = " ";
+//var x = " ";
 
-client.on('connect', function () {
-  client.subscribe('temperatura')
-  client.subscribe('humedad')
-  client.subscribe('velocidad')
-  client.publish('temperatura', messageTemp)
+//subscribe a los topicos de los sensores
+client.on('connect', function() {
+	client.subscribe('temperatura');
+	client.subscribe('humedad');
+	client.subscribe('velocidad');
+	client.subscribe('presion');
+	client.subscribe('puntoRocio');
+	client.subscribe('velViento');
+	client.subscribe('dirViento');
+	client.subscribe('mmAgua');
+	client.subscribe('UV');
+	client.subscribe('lummens');
+	//Publica de prueba
+	client.publish('temperatura', messageTemp);
 });
 
-
-client.on('message', function (topic, message) {
-  x = message.toString();
-  idmess = x.split(" ");
-  if(topic == "humedad"){
-    console.log("es humedad");
-  }
-  if(topic == "temperatura"){
-    console.log("es temperatura");
-  }
-  if(topic == "velocidad"){
-    console.log("es viento");
-  }
-  // message is Buffer
-  console.log(message.toString())
-  console.log(idmess[0]);
-  console.log(idmess[1]);
-  //client.end()
+//Carga de los datos obtenidos a mongoDB
+client.on('message', function(topic, message) {
+	//x = message.toString();
+	splitMessage = message.toString().split(" ");
+	var sensor = new Sensor({
+		paramSensor: String(topic),
+		dato: String(splitMessage[0]),
+		idTotem: String(splitMessage[1]),
+		fechaYHora: Date()
+	});
+	sensor.save(function(err) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log("los datos fueron cargados a la db");
+		}
+	})
 });
 
+//Puerto donde corre el sistema
 server.listen(8080);
 
+//Ruteo a las paginas
 app.use(express.static(__dirname + '/public'));
-
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/views/index.html');
+	res.sendFile(__dirname + '/views/index.html');
 });
-
 app.get('/chat', function(req, res) {
-    res.sendFile(__dirname + '/views/chat.html');
-
+	res.sendFile(__dirname + '/views/chat.html');
 });
 
+//Sistema de Chat
 io.sockets.on('connection', function(socket) {
-    socket.on('send message', function(data) {
-        io.sockets.emit('new message', {msg: data, nick: socket.nickname});
-    });
+	socket.on('send message', function(data) {
+		io.sockets.emit('new message', {
+			msg: data,
+			nick: socket.nickname
+		});
+	});
 
-    socket.on('new user', function(data, callback) {
-        if (data in nicknames) {
-            callback(false);
-        } else {
-            callback(true);
-            socket.nickname = data;
-            nicknames[socket.nickname] = 1;
-            updateNickNames();
-        }
-    });
+	socket.on('new user', function(data, callback) {
+		if (data in nicknames) {
+			callback(false);
+		} else {
+			callback(true);
+			socket.nickname = data;
+			nicknames[socket.nickname] = 1;
+			updateNickNames();
+		}
+	});
 
-    socket.on('disconnect', function(data) {
-        if(!socket.nickname) return;
-        delete nicknames[socket.nickname];
-        updateNickNames();
-    });
+	socket.on('disconnect', function(data) {
+		if (!socket.nickname) return;
+		delete nicknames[socket.nickname];
+		updateNickNames();
+	});
 
-    function updateNickNames() {
-        io.sockets.emit('usernames', nicknames);
-    }
+	function updateNickNames() {
+		io.sockets.emit('usernames', nicknames);
+	}
 });
